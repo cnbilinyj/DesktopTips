85b82da3bb2f4419575d8c5976ae7f347966e82b
cnbilinyj <wjrbilibilinyj@163.com>
2025-02-28 17:54:17 +0800

    使窗口在使用“显示桌面”功能时不被隐藏

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Threading;
 using System.Text.RegularExpressions;
 
 namespace DesktopTips
@@ -30,6 +27,7 @@ namespace DesktopTips
         public MainWindow()
         {
             InitializeComponent();
+            SendMsgToProgman();
             Loaded += MainWindow_Loaded;
         }
 
@@ -327,6 +325,54 @@ namespace DesktopTips
             exStyle |= (int)ExtendedWindowStyles.WS_EX_TOOLWINDOW;
 
             SetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE, (IntPtr)exStyle);
+        }
+
+    //Win32方法
+        public static class Win32Func
+        {
+            [DllImport("user32.dll")]
+            public static extern IntPtr FindWindow(string className, string? winName);
+    
+            [DllImport("user32.dll")]
+            public static extern IntPtr SendMessageTimeout(IntPtr hwnd, uint msg, IntPtr wParam, IntPtr lParam, uint fuFlage, uint timeout, IntPtr result);
+    
+            //查找窗口的委托 查找逻辑
+            public delegate bool EnumWindowsProc(IntPtr hwnd, IntPtr lParam);
+            [DllImport("user32.dll")]
+            public static extern bool EnumWindows(EnumWindowsProc proc, IntPtr lParam);
+    
+            [DllImport("user32.dll")]
+            public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string className, string? winName);
+    
+            [DllImport("user32.dll")]
+            public static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+    
+            [DllImport("user32.dll")]
+            public static extern IntPtr SetParent(IntPtr hwnd, IntPtr parentHwnd);
+        }
+
+        public void SendMsgToProgman()
+        {
+            // 桌面窗口句柄，在外部定义，用于后面将我们自己的窗口作为子窗口放入
+            nint programHandle = Win32Func.FindWindow("Progman", null);
+            IntPtr result = IntPtr.Zero;
+            // 向 Program Manager 窗口发送消息 0x52c 的一个消息，超时设置为2秒
+            Win32Func.SendMessageTimeout(programHandle, 0x52c, IntPtr.Zero, IntPtr.Zero, 0, 2, result);
+ 
+            // 遍历顶级窗口
+            Win32Func.EnumWindows((hwnd, lParam) =>
+            {
+                // 找到第一个 WorkerW 窗口，此窗口中有子窗口 SHELLDLL_DefView，所以先找子窗口
+                if (Win32Func.FindWindowEx(hwnd, IntPtr.Zero, "SHELLDLL_DefView", null) != IntPtr.Zero)
+                {
+                    // 找到当前第一个 WorkerW 窗口的，后一个窗口，及第二个 WorkerW 窗口。
+                    IntPtr tempHwnd = Win32Func.FindWindowEx(IntPtr.Zero, hwnd, "WorkerW", null);
+ 
+                    // 隐藏第二个 WorkerW 窗口
+                    Win32Func.ShowWindow(tempHwnd, 0);
+                }
+                return true;
+            }, IntPtr.Zero);
         }
     }
 }
